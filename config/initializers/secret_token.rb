04a634ab299388dbd669f1d97f8addd56e4fa9dc
Chu Anh Tuan <chu.anh.tuan@framgia.com>
2014-01-20 13:53:50 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrainingGit0102::Application.config.secret_key_base = 'ee99c71ec49f360daa7d538a4d53165349bf323f40b987b92d5fe988f69644dcd99130b518ae170688d96777f207feb5dc56106a393f2c4dd12bd3a0b7184ab6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrainingGit0102::Application.config.session_store :cookie_store, key: '_training_git_0102_session'
